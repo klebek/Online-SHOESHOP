c18c25ac94902e3edd90a978ec9a24ed53b6eb7c
klebek <s13928@pjwstk.edu.pl>
2018-05-12 21:11:01 +0200

    Navbar + GoogleLogin

---
+++ online-shop/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCgBxdLsbLsneQTeypaUtwkK7vE95gRyys',
+    authDomain: 'online-shop-69b8c.firebaseapp.com',
+    databaseURL: 'https://online-shop-69b8c.firebaseio.com',
+    projectId: 'online-shop-69b8c',
+    storageBucket: 'online-shop-69b8c.appspot.com',
+    messagingSenderId: '396726396725'
+
+  }
+};
