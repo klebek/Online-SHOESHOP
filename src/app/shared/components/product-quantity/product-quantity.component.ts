7b759ef1e57879273bf1f15e8ec2c1ee7001fbfa
klebek <s13928@pjwstk.edu.pl>
2018-09-28 16:42:39 +0200

    Modernised quantity component

--- src/app/shared/components/product-quantity/product-quantity.component.ts
+++ src/app/shared/components/product-quantity/product-quantity.component.ts
@@ -12,6 +12,7 @@ export class ProductQuantityComponent {
 
   @Input('product') product: Product;
   @Input('shopping-cart') shoppingCart: ShoppingCart;
+  @Input('inCart') inCart = false;
   
   constructor(private cartService: ShoppingCartService) { }
 
