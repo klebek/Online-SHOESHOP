652b2ee08972152b1173b36c2d8e59b4ea484b55
klebek <s13928@pjwstk.edu.pl>
2018-10-19 18:24:53 +0200

    new heroku

--- server.js
+++ server.js
@@ -2,10 +2,10 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/shoeshop'));
+app.use(express.static(__dirname + '/dist/online-shop'));
 
 app.all('*', (req, res) => {
-  res.status(200).sendFile(__dirname + '/dist/shoeshop/index.html');
+  res.status(200).sendFile(__dirname + '/dist/online-shop/index.html');
 });
 
 app.listen(process.env.PORT || 8080);
