924ca55760f67b9cbfa6dfc97dea23319585d6be
klebek <s13928@pjwstk.edu.pl>
2019-02-24 20:58:02 +0100

    Dropdown menu

--- src/app/core/components/dropdown-menus/dropdown-menus.component.ts
+++ src/app/core/components/dropdown-menus/dropdown-menus.component.ts
@@ -1,18 +1,24 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { AuthService } from 'shared/services/auth.service';
+import { ShoppingCartService } from 'shared/services/shopping-cart.service';
 
 @Component({
   selector: 'dropdown-menus',
   templateUrl: './dropdown-menus.component.html',
-  styleUrls: ['./dropdown-menus.component.css']
+  styleUrls: ['./dropdown-menus.component.scss']
 })
-export class DropdownMenusComponent {
+export class DropdownMenusComponent implements OnInit {
 
   @Input('appUser') appUser;
   @Input('hamburger') hamburger = false;
-  @Input('cart') cart;
 
-  constructor(private auth: AuthService) {}
+  cart$;
+
+  constructor(private auth: AuthService,private cartService: ShoppingCartService) {}
+  
+  async ngOnInit(){
+    this.cart$ = await this.cartService.getCart();
+  }
 
   logout() {
     this.auth.logout();
